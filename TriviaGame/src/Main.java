6ef4f99bbfb1b85caedb676d731b0a0b97582e7a
evasm_000 <evasm_000@192.168.128.198>
2017-02-19 02:59:21 -0800

    multiple rounds added

--- TriviaGame/src/Main.java
+++ TriviaGame/src/Main.java
@@ -26,14 +26,20 @@ public static void Main(String args[]){
 	int numPlayers=scan.nextInt();
 	Player[] gamePlayers= new Player[numPlayers];
 	ArrayList<Player> PlayersSortedByScore= new ArrayList<Player>();
+	int continueOrNot;
+	Player currentPlayer;
 	
 	for(int m=1; m>0; m++){
+		System.out.println();
+		System.out.println("ROUND "+m);
+		System.out.println();
 		
 	for(int i=0; i<numPlayers; i++){
 		System.out.println("It is player "+(i+1)+"'s turn!");
 		System.out.println("What is your name?");
-		Player currentPlayer= gamePlayers[i];
+		currentPlayer= gamePlayers[i];
 		currentPlayer.setName(scan.next());
+		System.out.println();
 		
 		Question PCQ = new PopCultureQuestion();
 		System.out.println("Question 1: Pop Culture:"+ PCQ.askQuestion()); 
@@ -42,6 +48,7 @@ public static void Main(String args[]){
 			currentPlayer.incrementPoints();
 			System.out.println("Correct!");}
 		else{System.out.println("Incorrect. The correct answer is: "+ PCQ.getAnswer());}
+		System.out.println();
 		
 		Question MathQ = new MathQuestion();
 		System.out.println("Question 1:Math:"+ MathQ.askQuestion()); 
@@ -50,6 +57,7 @@ public static void Main(String args[]){
 			currentPlayer.incrementPoints();
 			System.out.println("Correct!");}
 		else{System.out.println("Incorrect. The correct answer is: "+ MathQ.getAnswer());}
+		System.out.println();
 		
 		Question HistoryQ = new HistoryQuestion();
 		System.out.println("Question 2:History:"+ HistoryQ.askQuestion()); 
@@ -58,6 +66,7 @@ public static void Main(String args[]){
 			currentPlayer.incrementPoints();
 			System.out.println("Correct!");}
 		else{System.out.println("Incorrect. The correct answer is: "+ HistoryQ.getAnswer());}
+		System.out.println();
 		
 		Question ScienceQ = new ScienceQuestion();
 		System.out.println("Question 1:Math:"+ ScienceQ.askQuestion()); 
@@ -66,6 +75,7 @@ public static void Main(String args[]){
 			currentPlayer.incrementPoints();
 			System.out.println("Correct!");}
 		else{System.out.println("Incorrect. The correct answer is: "+ ScienceQ.getAnswer());}
+		System.out.println();
 		
 		Question GeographyQ = new GeographyQuestion();
 		System.out.println("Question 1:Math:"+ GeographyQ.askQuestion()); 
@@ -74,17 +84,28 @@ public static void Main(String args[]){
 			currentPlayer.incrementPoints();
 			System.out.println("Correct!");}
 		else{System.out.println("Incorrect. The correct answer is: "+ GeographyQ.getAnswer());}
+		System.out.println();
 		
 		gamePlayers[i]=currentPlayer;
 		}
 		System.out.println("Round "+m+" complete! Here are the current rankings:");
+		PlayersSortedByScore= sortScores(gamePlayers);
+		for(Player g:PlayersSortedByScore){
+			System.out.println((PlayersSortedByScore.indexOf(g)+1)+". "+g.getName()+" ----- "+g.getPoints()+" points.");
+		}
+		System.out.println();
+		System.out.println("Do you want to continue your game or end it here?");
+		System.out.println("To continue you game, type '1', to end it now type '2'.");
+		continueOrNot=scan.nextInt();
+		if(continueOrNot==2){m=-1;}
 		
 	}
 	PlayersSortedByScore= sortScores(gamePlayers);
-	
+	System.out.println("Game ended.");
+	System.out.println();
 	System.out.println("The Winner is: "+PlayersSortedByScore.get(0).getName()+"!");
 	System.out.println();
-	System.out.println("Here are the rankings:");
+	System.out.println("Here are the final rankings:");
 	for(Player g:PlayersSortedByScore){
 		System.out.println((PlayersSortedByScore.indexOf(g)+1)+". "+g.getName()+" ----- "+g.getPoints()+" points.");
 	}
